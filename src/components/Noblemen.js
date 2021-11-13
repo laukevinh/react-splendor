bd10ea16e4d225e55763d3197dd0715b0be0cfc6
klau <laudevelopment@gmail.com>
2021-11-12 23:32:45 -0600

    Change Noblemen to functional components

--- src/components/Noblemen.js
+++ src/components/Noblemen.js
@@ -1,131 +1,76 @@
 import React from 'react';
-import { Grid, Modal } from 'semantic-ui-react';
-import Price from '../objects/Price';
-
+import { Card, Grid, Modal } from 'semantic-ui-react';
 import renderPrice from '../utils'
 
-var VP = 3;
-
-// var nobles = [
-//   [VP, 3, 3, 3, 0, 0],
-//   [VP, 0, 3, 3, 3, 0],
-//   [VP, 0, 0, 3, 3, 3],
-//   [VP, 3, 0, 0, 3, 3],
-//   [VP, 3, 3, 0, 0, 3],
-//   [VP, 4, 4, 0, 0, 0],
-//   [VP, 0, 4, 4, 0, 0],
-//   [VP, 0, 0, 4, 4, 0],
-//   [VP, 0, 0, 0, 4, 4],
-//   [VP, 4, 0, 0, 0, 4]
-// ];
-var nobles = [
-  [VP, 1, 0, 0, 0, 0],
-  [VP, 0, 1, 0, 0, 0],
-  [VP, 0, 0, 1, 0, 0],
-  [VP, 0, 0, 0, 1, 0],
-  [VP, 0, 0, 0, 0, 1],
-  [VP, 1, 0, 0, 0, 0],
-  [VP, 0, 1, 0, 0, 0],
-  [VP, 0, 0, 1, 0, 0],
-  [VP, 0, 0, 0, 1, 0],
-  [VP, 0, 0, 0, 0, 1],
-];
-
-class NobleBase {
-  constructor(points, white, blue, green, red, black, isDisplayed) {
-    this.points = points;
-    this.price = new Price(white, blue, green, red, black);
-    this.isDisplayed = isDisplayed;
-  }
-}
-
-export const allNoblemen = nobles.map(([points, white, blue, green, red, black]) => new NobleBase(points, white, blue, green, red, black, true));
-
 function Noble(props) {
-  const isDisplayed = props.isDisplayed;
-  const selectable = props.selectable ? "selectable" : "";
-  const classNames = ["noble", selectable].join(" ");
-  return isDisplayed ? (
-    <div className={classNames}>
-      <Grid.Row>
-        VP: {props.points}
-      </Grid.Row>
+  const content = (
+    <Card.Content>
+      <Card.Header textAlign='right' as='h1'>
+        {props.points}
+      </Card.Header>
       {renderPrice(props.price, "game-card")}
-    </div>
+    </Card.Content>
+  );
+  return props.handleNoblemenSelection ? (
+    <Card onClick={() => props.handleNoblemenSelection(props.idx)}>
+      {content}
+    </Card>
   ) : (
-    <></>
+    <Card>
+      {content}
+    </Card>
   );
 }
 
-export default class Noblemen extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const noblemen = this.props.noblemen.map(noble => {
-      return (
-        <Grid.Column>
+export default function Noblemen(props) {
+  const { noblemen } = props;
+  const nobles = noblemen.map(noble => {
+    return (
+      <Grid.Column>
+        {
+          noble.isDisplayed &&
           <Noble
             points={noble.points}
             price={noble.price}
-            isDisplayed={noble.isDisplayed}
           />
-        </Grid.Column>
-      );
-    });
-
-    return (
-      <Grid columns={this.props.noblemen.length}>
-        {noblemen}
-      </Grid>
+        }
+      </Grid.Column>
     );
-  }
+  });
+
+  return (
+    <Grid columns={nobles.length}>
+      {nobles}
+    </Grid>
+  );
 }
 
-export class ModalNoblemen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: props.open,
-    };
-  }
-
-  handleConfirm(nobleIndex) {
-    this.setState({ open: false });
-    this.props.handleNoblemenSelection(nobleIndex);
-  }
-
-  handleSelect(index) {
-    this.setState({ selectedNoble: index });
-  }
-
-  render() {
-    // noblemen only contains the ones you're qualified to select
-    const noblemen = this.props.selectableNoblemen.map((selectable, idx) => {
-      const noble = this.props.noblemen[idx];
-      return selectable ? (
-        <Grid.Column onClick={() => this.handleConfirm(idx)}>
+export function ModalNoblemen(props) {
+  const { noblemen, selectableNoblemen, handleNoblemenSelection, open } = props;
+  const nobles = noblemen.map((noble, idx) => {
+    return (
+      <Grid.Column>
+        {
+          selectableNoblemen[idx] &&
           <Noble
+            idx={idx}
             points={noble.points}
             price={noble.price}
-            isDisplayed={noble.isDisplayed}
-            selectable
+            handleNoblemenSelection={handleNoblemenSelection}
           />
-        </Grid.Column>
-      ) : (
-        <Grid.Column></Grid.Column>
-      )
-    });
-
-    return (
-      <Modal open={this.props.open}>
-        <Modal.Header>Select Noble</Modal.Header>
-        <Modal.Content>
-          <Grid columns={noblemen.length}>
-            {noblemen}
-          </Grid>
-        </Modal.Content>
-      </Modal>
+        }
+      </Grid.Column>
     );
-  }
+  });
+
+  return (
+    <Modal open={open}>
+      <Modal.Header>Select Noble</Modal.Header>
+      <Modal.Content>
+        <Grid columns={noblemen.length}>
+          {nobles}
+        </Grid>
+      </Modal.Content>
+    </Modal>
+  );
 }
