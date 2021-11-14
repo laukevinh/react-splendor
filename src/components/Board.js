1a3f314727bf1d69413175b57fdde85af41534a7
klau <laudevelopment@gmail.com>
2021-11-14 08:28:57 -0600

    Convert Board to function component

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,58 +2,53 @@ import React from 'react';
 import { Grid, Card } from 'semantic-ui-react';
 import CardModal from './CardModal';
 import { DECK, BOARD } from '../utils';
+import { MAX_BOARD_COLS, MAX_BOARD_ROWS } from '../constants/defaults';
 
-export default class Board extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    let cards = this.props.cards;
-    let rows = [];
-    for (let level = 0; level < 3; level++) {
-      let deck = this.props.decks[level];
-      let deckModal = (
+export default function Board(props) {
+  let cards = props.cards;
+  let rows = [];
+  for (let level = 0; level < MAX_BOARD_ROWS; level++) {
+    let deck = props.decks[level];
+    let deckModal = (
+      <CardModal
+        source={DECK}
+        level={level}
+        deck={deck}
+        handleReserve={props.handleReserve}
+        finished={props.finished}
+      />
+    );
+    let cols = [];
+    for (let col = 0; col < MAX_BOARD_COLS; col++) {
+      let card = cards[level][col];
+      cols.push(
         <CardModal
-          source={DECK}
+          source={BOARD}
           level={level}
-          deck={deck}
-          handleReserve={this.props.handleReserve}
-          finished={this.props.finished}
+          column={col}
+          card={card}
+          playerWallet={props.playerWallet}
+          playerCards={props.playerCards}
+          handleBuy={props.handleBuy}
+          handleReserve={props.handleReserve}
+          finished={props.finished}
         />
       );
-      let cols = [];
-      for (let col = 0; col < 4; col++) {
-        let card = cards[level][col];
-        cols.push(
-          <CardModal
-            source={BOARD}
-            level={level}
-            column={col}
-            card={card}
-            playerWallet={this.props.playerWallet}
-            playerCards={this.props.playerCards}
-            handleBuy={this.props.handleBuy}
-            handleReserve={this.props.handleReserve}
-            finished={this.props.finished}
-          />
-        );
-      }
-      rows.push(
-        <Grid.Row columns={2}>
-          <Grid.Column width={1}>
-            {deckModal}
-          </Grid.Column>
-          <Grid.Column width={15}>
-            <Card.Group itemsPerRow={4}>
-              {cols}
-            </Card.Group>
-          </Grid.Column>
-        </Grid.Row>
-      );
     }
-    return (
-      <Grid>{rows}</Grid>
+    rows.push(
+      <Grid.Row columns={2}>
+        <Grid.Column width={1}>
+          {deckModal}
+        </Grid.Column>
+        <Grid.Column width={15}>
+          <Card.Group itemsPerRow={4}>
+            {cols}
+          </Card.Group>
+        </Grid.Column>
+      </Grid.Row>
     );
   }
+  return (
+    <Grid>{rows}</Grid>
+  );
 }
